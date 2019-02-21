16ba29eb38d0f10c7176d9e6792c9feda838f162
Maxim Prokhorov <prokhorov.max@outlook.com>
2019-02-21 03:29:20 +0300

    Remove pyserial path argument from the upload script

--- tools/upload.py
+++ tools/upload.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python
 
 # Wrapper for Arduino core / others that can call esptool.py possibly multiple times
-# Adds pyserial to sys.path automatically based on the path of the current file
 
-# First patameter is pyserial path, then a series of command arguments separated with --end
-# i.e. upload.py tools/pyserial tools/esptool/esptool.py erase_flash --end write_flash file 0x0 --end
+# First patameter is esptool.py path, then a series of command arguments separated with --end
+# i.e. upload.py tools/esptool/esptool.py erase_flash --end write_flash file 0x0 --end
 
-import inspect
-import os
 import sys
+import subprocess
 
 sys.argv.pop(0) # Remove executable name
 try:
-    sys.path.append(sys.argv.pop(0).replace('\\', '/')) # Add pyserial dir to search path, in UNIX format
     esptool = sys.argv.pop(0).replace('\\', '/') # Full path to esptool.py, in UNIX format
 except:
-    sys.stderr.write("Error in command line, need pyserial path as 1st arg and esptool path as 2nd.\n")
+    sys.stderr.write("Error in command line, need esptool.py path.\n")
     sys.exit(1)
 
 fakeargs = [];
 while len(sys.argv):
     if sys.argv[0] == '--end':
-        pid  = os.fork()
-        if pid == 0:
-            sys.argv = ['esptool.py'] + fakeargs
-            sys.stderr.write("Running: " + " ".join(sys.argv) + "\n")
-            exec(open(esptool).read())
-            sys.exit(0)
-        else:
-            os.waitpid(pid, 0)
+        args = [sys.executable, esptool] + fakeargs
+        sys.stderr.write("Running: " + " ".join(args) + "\n")
+        if not subprocess.call(args):
+            sys.exit(1)
         sys.argv.pop(0) # Remove --end
         fakeargs = []
     else:
