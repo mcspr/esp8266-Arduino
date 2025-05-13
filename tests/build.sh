54f6ad0267df6fe90ea3d3821bed5c794f27acb0
Maxim Prokhorov <prokhorov.max@outlook.com>
2025-05-14 00:40:55 +0300

    expect $HOME in .sh, not from workflow
    currently, expansion does not happen and it is raw path $(pwd)/'${HOME}'/...

--- tests/build.sh
+++ tests/build.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 
+# expect to have git available
 root=$(git rev-parse --show-toplevel)
 
+# general configuration related to the builder itself
 ESP8266_ARDUINO_BUILD_DIR=${ESP8266_ARDUINO_BUILD_DIR:-$root}
 ESP8266_ARDUINO_BUILDER=${ESP8266_ARDUINO_BUILDER:-arduino}
 ESP8266_ARDUINO_PRESERVE_CACHE=${ESP8266_ARDUINO_PRESERVE_CACHE:-}
 
-ESP8266_ARDUINO_CLI=${ESP8266_ARDUINO_CLI:-$HOME/.local/bin/arduino-cli}
-ESP8266_ARDUINO_HARDWARE=${ESP8266_ARDUINO_HARDWARE:-$HOME/Arduino/hardware}
-ESP8266_ARDUINO_LIBRARIES=${ESP8266_ARDUINO_LIBRARIES:-$HOME/Arduino/libraries}
-
+# sketch build options
 ESP8266_ARDUINO_DEBUG=${ESP8266_ARDUINO_DEBUG:-nodebug}
 ESP8266_ARDUINO_LWIP=${ESP8266_ARDUINO_LWIP:-default}
 ESP8266_ARDUINO_SKETCHES=${ESP8266_ARDUINO_SKETCHES:-}
+
+ESP8266_ARDUINO_CLI=${ESP8266_ARDUINO_CLI:-$HOME/.local/bin/arduino-cli}
+
+# ref. https://arduino.github.io/arduino-cli/1.2/configuration/#default-directories
+case "${RUNNER_OS-}" in
+"Linux")
+    ESP8266_ARDUINO_HARDWARE=${ESP8266_ARDUINO_HARDWARE:-$HOME/Arduino/hardware}
+    ESP8266_ARDUINO_LIBRARIES=${ESP8266_ARDUINO_LIBRARIES:-$HOME/Arduino/libraries}
+    ;;
+"macOS")
+    ESP8266_ARDUINO_HARDWARE=${ESP8266_ARDUINO_HARDWARE:-$HOME/Documents/Arduino/hardware}
+    ESP8266_ARDUINO_LIBRARIES=${ESP8266_ARDUINO_LIBRARIES:-$HOME/Documents/Arduino/libraries}
+    ;;
+"Windows")
+    ESP8266_ARDUINO_HARDWARE=${ESP8266_ARDUINO_HARDWARE:-$HOME/Documents/Arduino/hardware}
+    ESP8266_ARDUINO_LIBRARIES=${ESP8266_ARDUINO_LIBRARIES:-$HOME/Documents/Arduino/libraries}
+    ;;
+*)
+    echo 'Unknown ${RUNNER_OS} = "' ${RUNNER_OS} '"'
+    exit 2
+esac
 
 source "$root/tests/common.sh"
 
